5283fd0d99ad3d8de60b7466515996f54f0e428e
tungnguyentu <tungnguyentu@bizflycloud.vn>
2021-09-24 13:38:17 +0700

    added app

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
 def main():
-    pass
+    return "<html>helo nguyentutung</html>"
 
 if __name__ == "__main__":
-    main()
+    app.run()
